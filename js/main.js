12aeea6671093930a8e58ea0577ca9626b263d66
Lawrence O. Caulker <locaulker@gmail.com>
2019-06-12 09:00:07 -0400

    team sectio top completed: added owl carousel slider

--- js/main.js
+++ js/main.js
@@ -4,6 +4,22 @@ var window;
           Preloader
 ================================== */
 $(window).on('load', function () {
-  $("#status").fadeOut(500);
-  $("#preloader").delay(350).fadeOut(1000);
+	$("#status").fadeOut(500);
+	$("#preloader").delay(350).fadeOut('slow');
+});
+
+/* ===============================================
+          Team Section: Owl Carousel
+=============================================== */
+$(function() { // jQuery's document.ready method
+	$("#team-members").owlCarousel({
+		items: 2,
+		autoplay: true,
+		smartSpeed: 700,
+		loop: true,
+		autoplayHoverPause: true,
+		nav: true,
+		dots: false,
+		navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>']
+	});
 });
