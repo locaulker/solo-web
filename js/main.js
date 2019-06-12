9ebebc911c7068294897680c80c95ff045b45448
Lawrence O. Caulker <locaulker@gmail.com>
2019-06-12 17:13:13 -0400

    Waypoints pluging added | Team section completed

--- js/main.js
+++ js/main.js
@@ -4,22 +4,40 @@ var window;
           Preloader
 ================================== */
 $(window).on('load', function () {
-	$("#status").fadeOut(500);
-	$("#preloader").delay(350).fadeOut('slow');
+  $("#status").fadeOut(500);
+  $("#preloader").delay(350).fadeOut('slow');
 });
 
-/* ===============================================
+
+/* =======================================
           Team Section: Owl Carousel
-=============================================== */
-$(function() { // jQuery's document.ready method
-	$("#team-members").owlCarousel({
-		items: 2,
-		autoplay: true,
-		smartSpeed: 700,
-		loop: true,
-		autoplayHoverPause: true,
-		nav: true,
-		dots: false,
-		navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>']
-	});
+======================================= */
+$(function () { // jQuery's document.ready method
+  $("#team-members").owlCarousel({
+    items: 2,
+    autoplay: true,
+    smartSpeed: 700,
+    loop: true,
+    autoplayHoverPause: true,
+    nav: true,
+    dots: false,
+    navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>']
+  });
+});
+
+
+/* ==============================================
+          Team Section: Progress bars/Waoypoints
+============================================== */
+$(function () {
+  $("#progress-elements").waypoint(function () {
+    $(".progress-bar").each(function () {
+      $(this).animate({
+        width: $(this).attr("aria-valuenow") + '%'
+      }, 1000);
+    });
+    this.destroy();
+  }, {
+    offset: 'bottom-in-view'
+  });
 });
